3ad984b25e61d72ca5d733e9c909ef82804fe542
Amr Waheed <amrmohamed877@gmail.com>
2024-09-18 10:16:13 +0300

    ..

--- JS-Day4-tasks/task3.js
+++ JS-Day4-tasks/task3.js
@@ -31,7 +31,7 @@ function handleSubmit(event) {
                     .details {
                         font-size: 18px;
                         margin-bottom: 20px;
-                        display:inline-block
+                        display: inline-block;
                     }
                     .question {
                         font-size: 20px;
@@ -46,7 +46,57 @@ function handleSubmit(event) {
                         display: block;
                         margin-bottom: 5px;
                     }
+                    .submit-button:disabled {
+                        background-color: gray;
+                        cursor: not-allowed;
+                    }
+                    .result {
+                        font-size: 20px;
+                        margin-top: 20px;
+                        font-weight: bold;
+                    }
+                    .correct {
+                        color: green;
+                    }
+                    .incorrect {
+                        color: red;
+                    }
                 </style>
+                <script>
+                    const correctAnswers = ['answer2', 'answer3', 'answer1']; // Correct answers for questions
+
+                    function checkAnswers() {
+                        const question1Answered = document.querySelector('input[name="answer1"]:checked') !== null;
+                        const question2Answered = document.querySelector('input[name="answer2"]:checked') !== null;
+                        const question3Answered = document.querySelector('input[name="answer3"]:checked') !== null;
+                        const submitButton = document.querySelector('.submit-button');
+                        
+                        submitButton.disabled = !(question1Answered && question2Answered && question3Answered);
+                    }
+
+                    function calculateScore() {
+                        let score = 0;
+                        const userAnswers = [
+                            document.querySelector('input[name="answer1"]:checked').value,
+                            document.querySelector('input[name="answer2"]:checked').value,
+                            document.querySelector('input[name="answer3"]:checked').value
+                        ];
+
+                        const resultDiv = document.querySelector('.result');
+                        resultDiv.innerHTML = '';
+
+                        for (let i = 0; i < correctAnswers.length; i++) {
+                            if (userAnswers[i] === correctAnswers[i]) {
+                                score++;
+                                resultDiv.innerHTML += '<p class="correct">Question ' + (i + 1) + ': Correct</p>';
+                            } else {
+                                resultDiv.innerHTML += '<p class="incorrect">Question ' + (i + 1) + ': Incorrect</p>';
+                            }
+                        }
+
+                        resultDiv.innerHTML += '<p>Your final score: ' + score + ' out of ' + correctAnswers.length + '</p>';
+                    }
+                </script>
             </head>
             <body>
                 <div class="container">
@@ -58,31 +108,32 @@ function handleSubmit(event) {
                     <div class="question">
                         <p>Question 1: Title</p>
                         <div class="answers">
-                            <label><input type="radio" name="answer1" value=""> answer1</label>
-                            <label><input type="radio" name="answer1" value=""> answer2</label>
-                            <label><input type="radio" name="answer1" value=""> answer3</label>
-                            <label><input type="radio" name="answer1" value=""> answer4</label>
+                            <label><input type="radio" name="answer1" value="answer1" onclick="checkAnswers()"> answer1</label>
+                            <label><input type="radio" name="answer1" value="answer2" onclick="checkAnswers()"> answer2</label>
+                            <label><input type="radio" name="answer1" value="answer3" onclick="checkAnswers()"> answer3</label>
+                            <label><input type="radio" name="answer1" value="answer4" onclick="checkAnswers()"> answer4</label>
                         </div>
                     </div>
                     <div class="question">
                         <p>Question 2: Title</p>
                         <div class="answers">
-                            <label><input type="radio" name="answer2" value=""> answer1</label>
-                            <label><input type="radio" name="answer2" value=""> answer2</label>
-                            <label><input type="radio" name="answer2" value=""> answer3</label>
-                            <label><input type="radio" name="answer2" value=""> answer4</label>
+                            <label><input type="radio" name="answer2" value="answer1" onclick="checkAnswers()"> answer1</label>
+                            <label><input type="radio" name="answer2" value="answer2" onclick="checkAnswers()"> answer2</label>
+                            <label><input type="radio" name="answer2" value="answer3" onclick="checkAnswers()"> answer3</label>
+                            <label><input type="radio" name="answer2" value="answer4" onclick="checkAnswers()"> answer4</label>
                         </div>
                     </div>
                     <div class="question">
                         <p>Question 3: Title</p>
                         <div class="answers">
-                            <label><input type="radio" name="answer3" value=""> answer1 </label>
-                            <label><input type="radio" name="answer3" value=""> answer2</label>
-                            <label><input type="radio" name="answer3" value=""> answer3</label>
-                            <label><input type="radio" name="answer3" value=""> answer4</label>
+                            <label><input type="radio" name="answer3" value="answer1" onclick="checkAnswers()"> answer1</label>
+                            <label><input type="radio" name="answer3" value="answer2" onclick="checkAnswers()"> answer2</label>
+                            <label><input type="radio" name="answer3" value="answer3" onclick="checkAnswers()"> answer3</label>
+                            <label><input type="radio" name="answer3" value="answer4" onclick="checkAnswers()"> answer4</label>
                         </div>
                     </div>
-                    <button class="submit-button" onclick="window.close()">Submit</button>
+                    <button class="submit-button" disabled onclick="calculateScore()">Submit</button>
+                    <div class="result"></div>
                 </div>
             </body>
             </html>
